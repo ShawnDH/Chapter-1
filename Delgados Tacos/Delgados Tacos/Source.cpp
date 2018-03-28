ec9a6008568302c5ae18390bfa30ebb10eba8998
ShawnDH <37811310+ShawnDH@users.noreply.github.com>
2018-03-28 12:40:19 -0500

    update Delgados Tacos

---
+++ Delgados Tacos/Delgados Tacos/Source.cpp
@@ -0,0 +1,31 @@
+//Delgados Tacos
+//03/28/2018
+//Author: ShawnD Herrick
+
+
+#include <iostream>
+#include<string>
+
+int main();
+{
+	double tacos = 0.99;
+	double burritos = 1.99;
+	double enchiladas = 2.99;
+	double tostadoas = 1.50;
+	double drink = 0.99;
+	double churro = 1.25;
+	double supremeBurrito = 4.50;
+	double potatoOllas = 1.99;
+	double frEShAVacaDo = 9.99;
+
+	std::cout << "      __________      " << std::endl;
+	std::cout << "     /          \     " << std::endl;
+	std::cout << "    /   <   >    \ \  " << std::endl;
+	std::cout << "   |    _____     |\  " << std::endl;
+	std::cout << "   |    \___/     |\  " << std::endl;
+	std::cout << "    \______________\/ " << std::endl;
+
+	system("pause");
+	return 0;
+
+}
